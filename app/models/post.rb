a6dd38a73a0faa4918ff9349356fbe80b964f66d
Naomi Dennis <njdennis94@gmail.com>
2018-04-17 15:03:47 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, presence: true, allow_blank: false
+  validates :content, length: {minimum: 100}
   validates :category, presence: true
 end
