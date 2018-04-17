960e8273b68e312dc8daf0f2bb6c05e04e95f99f
Naomi Dennis <njdennis94@gmail.com>
2018-04-17 15:22:57 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,9 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create!(author_params)
-
+    if !@author.valid? 
+      render :edit 
+    end 
     redirect_to author_path(@author)
   end
 
